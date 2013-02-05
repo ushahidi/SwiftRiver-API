c01f436c04557c89e559ba314f33b636cd1e50f2
Brian Muita <bgm@69mb.org>
2013-02-05 18:41:17 +0300

    Set connection timezone to UTC in test data script.

--- src/test/java/com/ushahidi/swiftriver/core/api/controller/AccountsControllerTest.java
+++ src/test/java/com/ushahidi/swiftriver/core/api/controller/AccountsControllerTest.java
@@ -51,7 +51,7 @@ public class AccountsControllerTest extends AbstractControllerTest {
 				.andExpect(jsonPath("$.account_path").value("user1"))
 				.andExpect(jsonPath("$.active").value(true))
 				.andExpect(jsonPath("$.public").value(true))
-				.andExpect(jsonPath("$.date_added").value("Mon, 31 Dec 2012 21:00:02 +0000"))
+				.andExpect(jsonPath("$.date_added").value("Tue, 1 Jan 2013 00:00:02 +0000"))
 				.andExpect(jsonPath("$.river_quota_remaining").value(20))
 				.andExpect(jsonPath("$.follower_count").value(2))
 				.andExpect(jsonPath("$.following_count").value(1))
@@ -61,7 +61,7 @@ public class AccountsControllerTest extends AbstractControllerTest {
 				.andExpect(jsonPath("$.owner.name").value("User 1"))
 				.andExpect(jsonPath("$.owner.email").value("user1@myswiftriver.com"))
 				.andExpect(jsonPath("$.owner.username").value("user1"))
-				.andExpect(jsonPath("$.owner.date_added").value("Mon, 31 Dec 2012 21:00:02 +0000"))
+				.andExpect(jsonPath("$.owner.date_added").value("Tue, 1 Jan 2013 00:00:02 +0000"))
 				.andExpect(jsonPath("$.rivers").exists())
 				.andExpect(jsonPath("$.rivers[0].id").value(1))
 				.andExpect(jsonPath("$.rivers[0].name").value("River 1"))
@@ -73,8 +73,8 @@ public class AccountsControllerTest extends AbstractControllerTest {
 				.andExpect(jsonPath("$.rivers[0].drop_count").value(100))
 				.andExpect(jsonPath("$.rivers[0].drop_quota").value(10000))
 				.andExpect(jsonPath("$.rivers[0].full").value(false))
-				.andExpect(jsonPath("$.rivers[0].date_added").value("Tue, 1 Jan 2013 21:00:02 +0000"))
-				.andExpect(jsonPath("$.rivers[0].expiry_date").value("Fri, 1 Feb 2013 21:00:02 +0000"))
+				.andExpect(jsonPath("$.rivers[0].date_added").value("Wed, 2 Jan 2013 00:00:02 +0000"))
+				.andExpect(jsonPath("$.rivers[0].expiry_date").value("Sat, 2 Feb 2013 00:00:02 +0000"))
 				.andExpect(jsonPath("$.rivers[0].extension_count").value(0))
 				.andExpect(jsonPath("$.buckets").exists())
 				.andExpect(jsonPath("$.buckets[0].id").value(1))
@@ -85,6 +85,6 @@ public class AccountsControllerTest extends AbstractControllerTest {
 				.andExpect(jsonPath("$.buckets[0].is_following").value(false))
 				.andExpect(jsonPath("$.buckets[0].public").value(true))
 				.andExpect(jsonPath("$.buckets[0].drop_count").value(13))
-				.andExpect(jsonPath("$.buckets[0].date_added").value("Tue, 1 Jan 2013 21:00:02 +0000"));
+				.andExpect(jsonPath("$.buckets[0].date_added").value("Wed, 2 Jan 2013 00:00:02 +0000"));
 	}
 }
