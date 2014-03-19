7dad82b978eb03e2b128764557e0a7a5f193ddd5
Diogo Freire <github@diogofreire.com>
2014-03-19 21:28:30 +1300

    Adding data for `seq` table

--- src/main/resources/config/sql/schema.sql
+++ src/main/resources/config/sql/schema.sql
@@ -778,3 +778,14 @@ VALUES
 INSERT INTO `swiftriver`.`users`
 VALUES
     ('1', 'myswiftriver@myswiftriver.com', 'Administrator', 'admin', '$2a$05$f0I9XjamKm4LEaF8av1Zy.tzBrzFM0smLMKvMAqUWicGAcEnkCdQe', '0', '1', '10', '2014-03-08 13:21:15', '2013-01-01 00:00:00', '0', '0');
+
+-- Core API needs these up or else it throws a Null Pointer exception
+INSERT INTO seq (name, id) VALUES
+  ('droplets', 10),
+  ('tags', 11),
+  ('places', 13),
+  ('links', 10),
+  ('identities', 2),
+  ('media', 10),
+  ('river_tag_trends', 11),
+  ('rivers_droplets', 5);
